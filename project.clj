a6048b71d24990a23e2d009afcaf1e6b5e4c3b7e
Carsten Behring <carsten.behring@gmail.com>
2013-11-03 13:35:30 +0100

    fixed project file

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject lein-karma "0.1.0"
   :description "A leiningen plugin for running javascript tests with karma"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :lein-release {:deploy-via :lein-install}
 
 
   :profiles {:dev {
